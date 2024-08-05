952ef310242fdbc73743ef14aca4efde2761f773
ybw0014 <i@guizhanss.com>
2024-08-05 11:37:42 -0400

    fix: use the correct energy usage of mob data infuser

--- src/main/java/net/guizhanss/fastmachines/items/machines/infinityexpansion/FastInfinityWorkbench.java
+++ src/main/java/net/guizhanss/fastmachines/items/machines/infinityexpansion/FastInfinityWorkbench.java
@@ -7,19 +7,27 @@ import org.bukkit.inventory.ItemStack;
 
 import io.github.mooy1.infinityexpansion.items.blocks.InfinityWorkbench;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.items.settings.IntRangeSetting;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 
 import net.guizhanss.fastmachines.FastMachines;
 import net.guizhanss.fastmachines.core.recipes.RawRecipe;
-import net.guizhanss.fastmachines.items.machines.generic.AInfinityMachine;
+import net.guizhanss.fastmachines.items.machines.generic.AbstractFastMachine;
 import net.guizhanss.fastmachines.utils.RecipeUtils;
 
-public final class FastInfinityWorkbench extends AInfinityMachine {
+public final class FastInfinityWorkbench extends AbstractFastMachine {
     private static final ItemStack CRAFT_ITEM = FastMachines.getLocalization().getItem(
         "CRAFT", Material.RESPAWN_ANCHOR);
 
+    private final IntRangeSetting energyPerUse = new IntRangeSetting(this, "energy-per-use", 0, 10_000_000,
+        Integer.MAX_VALUE);
+    private final IntRangeSetting energyCapacity = new IntRangeSetting(this, "energy-capacity", 0, 100_000_000,
+        Integer.MAX_VALUE);
+
     public FastInfinityWorkbench(SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(item, recipeType, recipe);
+
+        addItemSetting(energyPerUse, energyCapacity);
     }
 
     @Override
@@ -31,5 +39,17 @@ public final class FastInfinityWorkbench extends AInfinityMachine {
     @Override
     protected ItemStack getCraftItem() {
         return CRAFT_ITEM;
+    }
+
+    @Override
+    public int getEnergyPerUse() {
+        return FastMachines.getConfigService().isFastMachinesUseEnergy() ?
+            energyPerUse.getValue() : 0;
+    }
+
+    @Override
+    public int getCapacity() {
+        return FastMachines.getConfigService().isFastMachinesUseEnergy() ?
+            energyCapacity.getValue() : 0;
     }
 }
--- src/main/java/net/guizhanss/fastmachines/items/machines/infinityexpansion/FastMobDataInfuser.java
+++ src/main/java/net/guizhanss/fastmachines/items/machines/infinityexpansion/FastMobDataInfuser.java
@@ -7,19 +7,27 @@ import org.bukkit.inventory.ItemStack;
 
 import io.github.mooy1.infinityexpansion.items.mobdata.MobDataInfuser;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.items.settings.IntRangeSetting;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 
 import net.guizhanss.fastmachines.FastMachines;
 import net.guizhanss.fastmachines.core.recipes.RawRecipe;
-import net.guizhanss.fastmachines.items.machines.generic.AInfinityMachine;
+import net.guizhanss.fastmachines.items.machines.generic.AbstractFastMachine;
 import net.guizhanss.fastmachines.utils.RecipeUtils;
 
-public final class FastMobDataInfuser extends AInfinityMachine {
+public final class FastMobDataInfuser extends AbstractFastMachine {
     private static final ItemStack CRAFT_ITEM = FastMachines.getLocalization().getItem(
         "CRAFT", Material.LODESTONE);
 
+    private final IntRangeSetting energyPerUse = new IntRangeSetting(this, "energy-per-use", 0, 20_000,
+        Integer.MAX_VALUE);
+    private final IntRangeSetting energyCapacity = new IntRangeSetting(this, "energy-capacity", 0, 200_000,
+        Integer.MAX_VALUE);
+
     public FastMobDataInfuser(SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(item, recipeType, recipe);
+
+        addItemSetting(energyPerUse, energyCapacity);
     }
 
     @Override
@@ -31,5 +39,17 @@ public final class FastMobDataInfuser extends AInfinityMachine {
     @Override
     protected ItemStack getCraftItem() {
         return CRAFT_ITEM;
+    }
+
+    @Override
+    public int getEnergyPerUse() {
+        return FastMachines.getConfigService().isFastMachinesUseEnergy() ?
+            energyPerUse.getValue() : 0;
+    }
+
+    @Override
+    public int getCapacity() {
+        return FastMachines.getConfigService().isFastMachinesUseEnergy() ?
+            energyCapacity.getValue() : 0;
     }
 }
