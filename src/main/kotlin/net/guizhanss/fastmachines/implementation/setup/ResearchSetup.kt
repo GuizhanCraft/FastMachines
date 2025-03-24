30288fd9e14ac53ddc6d808a0d411a3b5447d615
ybw0014 <i@guizhanss.com>
2025-03-24 12:18:05 -0700

    feat: add research

--- src/main/kotlin/net/guizhanss/fastmachines/implementation/setup/ResearchSetup.kt
+++ src/main/kotlin/net/guizhanss/fastmachines/implementation/setup/ResearchSetup.kt
@@ -29,11 +29,31 @@ object ResearchSetup {
             FMItems.STACKED_ANCIENT_PEDESTAL,
         )
 
-//        MACHINES.addItems(
-//            FMItems.FAST_CRAFTING_TABLE,
-//            FMItems.FAST_ENHANCED_CRAFTING_TABLE,
-//            FMItems.FAST_ORE_WASHER,
-//        )
+        MACHINES.addItems(
+            // vanilla
+            FMItems.FAST_CRAFTING_TABLE,
+            FMItems.FAST_FURNACE,
+            // slimefun
+            FMItems.FAST_ENHANCED_CRAFTING_TABLE,
+            FMItems.FAST_GRIND_STONE,
+            FMItems.FAST_ARMOR_FORGE,
+            FMItems.FAST_ORE_CRUSHER,
+            FMItems.FAST_COMPRESSOR,
+            FMItems.FAST_SMELTERY,
+            FMItems.FAST_PRESSURE_CHAMBER,
+            FMItems.FAST_MAGIC_WORKBENCH,
+            FMItems.FAST_ORE_WASHER,
+            FMItems.FAST_TABLE_SAW,
+            FMItems.FAST_COMPOSTER,
+            FMItems.FAST_PANNING_MACHINE,
+            FMItems.FAST_JUICER,
+            FMItems.FAST_ANCIENT_ALTAR,
+            // infinity expansion
+            FMItems.FAST_INFINITY_WORKBENCH,
+            FMItems.FAST_MOB_DATA_INFUSER,
+            // slimeframe
+            FMItems.FAST_SLIMEFRAME_FOUNDRY,
+        )
     }
 
     private fun Research.addItems(vararg items: SlimefunItemStack) {
